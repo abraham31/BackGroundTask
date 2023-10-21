ffba096e9a14f4b417784cc6afdc50a5c486a197
Abraham Soto <abraham_soto10@outlook.com>
2023-10-20 21:38:57 -0700

    Agregar archivos de proyecto.

---
+++ BackGroundService/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BackGroundService.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendNotificationToAll(string message)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", message);
+        }
+    }
+}
---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using BackGroundService;
+using Microsoft.AspNetCore.Mvc;
+
+
+public class HomeController : Controller
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public HomeController(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public IActionResult Index()
+    {
+        // Aquí, se considera que el cliente está interactuando
+        // Llama a RegisterActivity para registrar la actividad
+        var inactivityService = _serviceProvider.GetRequiredService<InactivityService>();
+        inactivityService.RegisterActivity();
+
+        // Resto de la lógica del controlador
+        return View();
+    }
+}
